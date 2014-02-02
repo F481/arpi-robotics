66ec84570a703fa439441486a127b2902e0e341b
Fabian Frick <fabi.kcirf@gmail.com>
2014-02-02 21:26:13 +0100

    fixed multiple definition errors

--- arduino/libraries/ARPI_MPU6050/arpi_mpu6050.h
+++ arduino/libraries/ARPI_MPU6050/arpi_mpu6050.h
@@ -56,8 +56,8 @@ THE SOFTWARE.
 // for both classes must be in the include path of your project
 //#include "I2Cdev.h"
 
-#include "MPU6050_6Axis_MotionApps20.h"
-//#include "MPU6050.h" // not necessary if using MotionApps include file
+//#include "MPU6050_6Axis_MotionApps20.h"
+#include "MPU6050.h" // not necessary if using MotionApps include file
 
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
