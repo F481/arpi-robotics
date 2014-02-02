4f18c88e9f3c0a41475005caf8cbf9d58ac8456b
Fabian Frick <fabi.kcirf@gmail.com>
2014-02-02 20:47:24 +0100

    updated arpi_mpu6050 lib

--- arduino/libraries/ARPI_MPU6050/arpi_mpu6050.cpp
+++ arduino/libraries/ARPI_MPU6050/arpi_mpu6050.cpp
@@ -1,35 +1,35 @@
 #include "arpi_mpu6050.h"
 
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 mpu;
+//MPU6050 mpu(0x69); // <-- use for AD0 high
+bool blinkState = false;
 
-ARPI_MPU6050::ARPI_MPU6050() {
-	// class default I2C address is 0x68
-	// specific I2C addresses may be passed as a parameter here
-	// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
-	// AD0 high = 0x69
-	MPU6050 mpu;
-	//MPU6050 mpu(0x69); // <-- use for AD0 high
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+int16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
 
-	bool blinkState = false;
-
-	// MPU control/status vars
-	bool dmpReady = false;  // set true if DMP init was successful
-	uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-	uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-	uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-	uint16_t fifoCount;     // count of all bytes currently in FIFO
-	uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-	// orientation/motion vars
-	Quaternion q;           // [w, x, y, z]         quaternion container
-	VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-	VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-	VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-	VectorFloat gravity;    // [x, y, z]            gravity vector
-	float euler[3];         // [psi, theta, phi]    Euler angle container
-	float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
 	// packet structure for InvenSense teapot demo
 	uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+
+ARPI_MPU6050::ARPI_MPU6050() {
+	
 }
 
 
