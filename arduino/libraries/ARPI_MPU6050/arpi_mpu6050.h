9ee5ae0a798029dcec244c5778700ce00bbed5e6
Besnik Br <besnik.br@googlemail.com>
2014-02-05 21:52:05 +0100

    fixed indentation

--- arduino/libraries/ARPI_MPU6050/arpi_mpu6050.h
+++ arduino/libraries/ARPI_MPU6050/arpi_mpu6050.h
@@ -1,36 +1,37 @@
 #define MPU6050_INCLUDE_DMP_MOTIONAPPS20
 
 #ifndef ARPI_MPU6050_h
-#define ARPI_MPU6050_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
-	#include <Arduino.h>
-#else
-	#include <WProgram.h>
-	#include <pins_arduino.h>
-#endif
+   #define ARPI_MPU6050_h
 
-#include <helper_3dmath.h>
+   #if defined(ARDUINO) && ARDUINO >= 100
+   	#include <Arduino.h>
+   #else
+   	#include <WProgram.h>
+   	#include <pins_arduino.h>
+   #endif
 
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    #include "Wire.h"
-#endif
+   #include <helper_3dmath.h>
+
+   // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+   // is used in I2Cdev.h
+   #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+       #include "Wire.h"
+   #endif
 
 
-#define OUTPUT_READABLE_YAWPITCHROLL
+   #define OUTPUT_READABLE_YAWPITCHROLL
 
-#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+   #define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
 
-class ARPI_MPU6050 {
-	public:
-		ARPI_MPU6050();
-		int setup();
-		void run();
-		float getYaw();
-		float getPitch();
-		float getRoll();
-};
+   class ARPI_MPU6050 {
+   	public:
+   		ARPI_MPU6050();
+   		int setup();
+   		void run();
+   		float getYaw();
+   		float getPitch();
+   		float getRoll();
+   };
 
 #endif
